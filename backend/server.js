e60923030a4b3c7764d60b197de032ac4e9aa513
ultriti <yashashravi1@gmail.com>
2026-05-31 09:36:45 +0530

    day 11 - deployment render setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const  express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-// const user = require("./router/user.route")
+const user = require("./router/user.route")
 const file = require("./router/file.route")
 
 dotenv.config();
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// app.use("/api/user",user)
+app.use("/api/user",user)
 app.use("/api/file",file);
 
 app.listen(PORT, () => {
